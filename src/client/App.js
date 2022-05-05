0be1ba2b5a19e67c00828fc422043a2397df9462
james <james.jang@latiste.com>
2022-05-05 09:09:07 -0700

    init

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: ["./src/client/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/"
+  },
+  devServer: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/api": "http://localhost:5000"
+    },
+    client: { webSocketTransport: "ws" },
+    webSocketServer: "ws"
+  },
+  resolve: {
+    extensions: [".jsx", ".js", "*"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+      inject: false,
+      favicon: "./public/favicon.ico"
+    })
+  ]
+};
